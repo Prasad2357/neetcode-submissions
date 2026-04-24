4a8911f6d7d7dc5daf1da55a8276cb4d2b565f8b
Prasad Jagadale <prasadjagadale2357@gmail.com>
2026-04-24 11:13:32 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        int n = s.size();
+        int m = t.size();
+
+        if(n != m)  //size not matching
+        {
+            return false;
+        }
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        for (int i=0; i<n; i++)
+        {
+            if(s[i]!=t[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+};
