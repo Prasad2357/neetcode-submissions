b753f73c9324a7d78a8ac9dfab360b7ab0389a37
Prasad Jagadale <prasadjagadale2357@gmail.com>
2026-04-25 13:11:35 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        //Brute Force: Nested Loops
+
+        // vector <int> tar;
+        // int n = nums.size();
+        
+        // for(int i=0; i<n; i++)
+        // {
+        //     for(int j=i+1; j<n; j++)
+        //     {
+        //         if(target == nums[i]+nums[j])
+        //         {
+        //             tar.push_back(i);
+        //             tar.push_back(j);
+
+        //             return tar;
+        //         }
+        //     }  
+        // }
+        // return tar;
+
+
+        // Optimal : hashmap and diff
+
+        unordered_map <int,int> mpp; //value: index format
+        int n = nums.size();
+
+        for(int i=0; i<n; i++)
+        {
+            int diff = target - nums[i];
+
+            if( mpp.find(diff) != mpp.end())   //found the complement
+            {
+                return {mpp[diff], i};
+            }
+
+            mpp[nums[i]]=i;   // if no complement is found store in value: index format
+        }
+
+        return {};
+    }
+};
+
