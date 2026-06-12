ee5ff2105a664376cf2e061ffdf8ace8d05045ff
Prasad Jagadale <prasadjagadale2357@gmail.com>
2026-06-12 16:37:43 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.cpp
@@ -0,0 +1,95 @@
+class TrieNode {
+    public:
+        TrieNode * children[26];
+        bool isLeaf;
+
+        TrieNode(){
+            for(int i=0; i<26; i++)
+            {
+                children[i]= nullptr;
+            }
+            isLeaf = false;
+        }
+    
+};
+
+class WordDictionary {
+public:
+
+    TrieNode *root;
+    
+    WordDictionary() {
+        root = new TrieNode(); 
+    }
+
+    bool searchHelper(const string& word, int idx, TrieNode* node){
+        //backtracking
+
+        //base case
+        if( idx == word.length())
+        {
+            return node->isLeaf;
+        }
+
+        char c = word[idx];
+
+        if(c=='.')
+        {
+            for(int i =0; i< 26; i++)
+            {
+                //constraint
+                if(node->children[i]!= nullptr)
+                {
+                    //make choices
+                    TrieNode *child = node->children[i];   //move the pointer
+
+                    //explore recurse
+                    if(searchHelper(word, idx+1, child))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        else{
+
+            if(node->children[c- 'a']== nullptr)
+            {
+                return false;
+            }
+
+            return searchHelper(word, idx+1, node->children[c-'a']);   //child out os scope
+
+        }
+    }
+
+    
+    void addWord(string word) {
+        //same as insert func
+
+        TrieNode *curr = root;
+        for(char c: word)
+        {
+            if(curr->children[c- 'a'] == nullptr )
+            {
+                //create a new node
+                TrieNode *newNode = new TrieNode();
+
+                //keep the reference
+                curr->children[c -'a'] = newNode;
+            }
+            //move the curr pointer
+            curr = curr->children[c -'a'];
+        }
+        //mark end
+        curr->isLeaf = true;
+        
+    }
+    
+    bool search(string word) {
+        return searchHelper(word, 0, root);
+
+    }
+};
