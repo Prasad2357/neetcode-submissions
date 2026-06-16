5442bf21c23b8fb37d15fcdb7f8a034e84bb282b
Prasad Jagadale <prasadjagadale2357@gmail.com>
2026-06-17 00:37:59 +0530

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.cpp
@@ -0,0 +1,89 @@
+class Solution {
+    int row;
+    int col;
+    vector<vector<int>> ans;
+    vector<vector<bool>> pac;
+    vector<vector<bool>> atl;
+
+    void dfs(vector<vector<int>> &heights, int i, int j, vector<vector<bool>> &visited)
+    {
+        //mark visited
+        visited[i][j] = true;
+
+        int dir[4] [2] = {{1, 0}, {-1, 0}, {0,1}, {0,-1}};
+        
+        //travel in four directions
+        for(auto &it: dir)
+        {
+            int nr = i + it[0];
+            int nc = j + it[1];
+
+            //conditions
+            if(nr<0 || nc<0 || nr>= row || nc>=col)
+            {
+                continue;
+            }
+
+            if(visited[nr][nc])
+            {
+                continue;
+            }
+
+            if(heights[nr][nc] < heights[i][j])
+            {
+                continue;
+            }
+
+            dfs(heights, nr, nc, visited);
+        }
+
+    }
+
+
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        if(heights.empty())
+        {
+            return{};
+        }
+        row = heights.size();
+        col = heights[0].size();
+
+        pac = vector<vector<bool>>(row, vector<bool>(col,false));
+        atl = vector<vector<bool>>(row, vector<bool>(col,false));
+
+        //run for every cell
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                //atlantic
+                if(i== row-1 || j==col-1)
+                {
+                    dfs(heights, i, j, atl);
+                }
+
+                //pacific
+                if(i==0 || j==0)
+                {
+                    dfs(heights, i, j, pac);
+                }
+            }
+        }
+
+        //intersection for ans
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                if(atl[i][j] && pac[i][j])
+                {
+                    ans.push_back({i,j});
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+};
